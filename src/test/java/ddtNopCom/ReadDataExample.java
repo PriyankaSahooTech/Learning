1e740a322c56b83a4a6343208154ac1bd3c7e835
PriyankaSahooTech <sahoopr26priyanka@gmail.com>
2022-12-11 23:19:21 -0800

    AddedFrameowrk
    AddedFramework

---
+++ src/test/java/ddtNopCom/ReadDataExample.java
@@ -0,0 +1,19 @@
+package ddtNopCom;
+
+import org.testng.annotations.Test;
+
+import utility.Constants;
+
+public class ReadDataExample {
+
+	
+	@Test
+	public void sampleTest() {
+		
+		
+		System.out.println(Constants.URL);
+		
+	}
+	
+	
+}
